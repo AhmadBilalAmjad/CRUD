ac9d49c4b3cc275c5a37500c4a8ffa9a3a10ce05
Ahmad Bilal Amjad <ahmad.bilal.amjad@outlook.com>
2019-06-02 14:54:40 +0500

    server.js modified finalized Api

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 // External Modules
-const express = require("express");
-const app = express();
+const express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser");
+
+// Internal Modules
+const Task = require("./Api/models/crudModel"),
+  routes = require("./Api/routes/crudRoutes");
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/TodoDB");
+
+// Setting bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Registering the route
+routes(app);
 
 // Starting Server
 const port = process.env.PORT || 3000;
